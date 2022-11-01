71d121d0f9a41b3cacefcc23161e46f204d47d41
dasco69 <claude.proaudiovisuel@gmail.com>
2022-11-01 12:20:23 +0100

    feat: add textarea base

---
+++ components/Atoms/textarea/ATextAreaField.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex justify-center items-center flex-col w-1/3 mb-5">
+        <label 
+        :for="label"
+        class="block mb-2 text-title font-dynapuff text-blueDark dark:text-gray-300">
+            {{ label }}
+        </label>
+        <textarea 
+            :id="label" 
+            :rows="rows" 
+            class="bg-blueDark border border-gray-300 text-white text-sm rounded-default focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            :placeholder="placeholder"
+            ></textarea>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class  extends Vue {
+    @Prop() label!: string
+    @Prop() placeholder!: string
+    @Prop({ default: 4 }) rows?: number
+}
+</script>
+
+<style scoped>
+
+</style>

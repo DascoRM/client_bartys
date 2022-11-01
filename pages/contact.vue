28906e36b724efec6cc13e379516290062f01498
dasco69 <claude.proaudiovisuel@gmail.com>
2022-11-01 12:22:03 +0100

    feat: add organism inside page contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-        Contact
+        <OContact />
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-
-@Component
+//Organism
+import OContact from '@/components/Organisms/OContact.vue'
+@Component({
+    components: {
+        OContact
+    }
+})
 export default class ContactPage extends Vue {
     layout() {
         return 'split'

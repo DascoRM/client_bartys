5e9a38fd6ba4ce6e6bf28ab5f0198b1604a53dfd
dasco69 <claude.proaudiovisuel@gmail.com>
2022-11-01 12:20:44 +0100

    feat: add title base

---
+++ components/Atoms/title/ATitleBase.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1 class="text-big font-dynapuff" :class="`text-${colorText}`">{{ title }}</h1>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ATitleBase extends Vue {
+    @Prop() title!: string
+    @Prop({default: "white"}) colorText?: string
+}
+</script>
+
+<style scoped>
+
+</style>

df9df9cd7d4b6409a246fa2ff63a8a6b520421e2
dasco69 <claude.proaudiovisuel@gmail.com>
2022-11-02 10:33:56 +0100

    feat: add contact page

--- components/Organisms/OContact.vue
+++ components/Organisms/OContact.vue
@@ -1,9 +1,20 @@
 <template>
     <div class="flex justify-center">
-        <div class="bg-blueDark w-1/3 h-screen">
-            blue
+        <div class="bg-blueDark w-1/3 h-screen hidden md:block">
+            <div class="h-screen flex items-center">
+                <MOverlayLeft>
+                     <template v-slot:title>
+                        <ATitleBase title="Contact"/>
+                     </template>
+                     <template v-slot:body>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                        </p>
+                     </template>
+                </MOverlayLeft>
+            </div>
         </div>
-        <div class="bg-white w-2/3 h-screen">
+        <div class="bg-white h-screen w-full md:w-2/3">
             <MContact class="w-full" />
         </div>
     </div>
@@ -11,12 +22,16 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+//Atoms
+import ATitleBase from '@/components/Atoms/title/ATitleBase.vue'
 //Molecules
 import MContact from '@/components/Molecules/contact/MContact.vue'
-
+import MOverlayLeft from '@/components/Molecules/overlay/MOverlayLeft.vue'
 
 @Component({
     components: {
+        MOverlayLeft,
+        ATitleBase,
         MContact
     }
 })

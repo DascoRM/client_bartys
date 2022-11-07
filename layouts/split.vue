2573b975fad8503dbf1e69f96b63f81ec424af19
dasco69 <claude.proaudiovisuel@gmail.com>
2022-11-07 11:21:41 +0100

    fix: delete sidebar

--- layouts/split.vue
+++ layouts/split.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="">
-        <OSidebar class="z-40 fixed w-3/12 md:w-1/12" />
         <Nuxt class="z-0 relative top-0 right-0 w-full" />
         <MFooter />
     </div>
@@ -8,13 +7,10 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-//Organsims
-import OSidebar from '../components/Organisms/OSideBar.vue'
 //Molecules
 import MFooter from '~/components/Molecules/footer/MFooter.vue'
 @Component({
     components: {
-        OSidebar,
         MFooter
     }
 })

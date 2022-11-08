c188ee7b7e81c19bcddd0abc4ceb2a636241bc30
dasco69 <claude.proaudiovisuel@gmail.com>
2022-11-08 12:25:01 +0100

    feat: add element

--- components/Atoms/marker/AMarker.vue
+++ components/Atoms/marker/AMarker.vue
@@ -5,11 +5,26 @@
         >
         <l-icon :icon-url="icon.url" :icon-size="icon.size" />
         <l-popup @click.native="$emit('getPage', marker)">
-            <div class="flex justify-center">
+            <div class="flex justify-start mb-2">
                 <h1 class="font-bold">{{ marker.title }}</h1>
             </div>
+            <div class="mb-4">
+                <img class="w-44" :src="require('img/test.jpg')" alt="logo bartys" />
+            </div>
+            <div class="inline-flex items-center">
+                <div :class="itsOpen ? 'bg-green-400': 'bg-red-400'" class="mb-2 rounded-full w-max h-auto p-2 mr-2">
+                    <ATitleSmall :title="itsOpen ? 'Open': 'Close'" colorText="white" />
+                </div>
+                <div class="mb-2 bg-blue-400 rounded-full w-max h-auto p-2 mr-2">
+                    <ATitleSmall title="4 Évenements" colorText="white" />
+                </div>
+            </div>
             <div>
+                <ATitleSmall title="Horaires: " />
                 <p>{{ marker.hours_opening }}</p>
+            </div>
+            <div class="h-10">
+                <AButtonRedirect title="Plus d'info" :to='`/bar/${marker.id}`' color="shadow-button bg-blueLight px-2" />
             </div>
         </l-popup>
     </l-marker>
@@ -21,17 +36,28 @@ import { Component, Vue, Prop } from 'vue-property-decorator';
 import { AMarkerType, IconMarker } from '../../../store/DTO/dtoComponent';
 //Atoms
 import { LMarker, LIcon, LPopup } from 'vue2-leaflet'
+import ATitleSmall from '@/components/Atoms/title/ATitleSmall.vue'
+import AButtonRedirect from '@/components/Atoms/button/AButtonRedirect.vue'
 
 @Component({
     components: {
         LMarker,
         LIcon,
-        LPopup
+        LPopup,
+        ATitleSmall,
+        AButtonRedirect
     }
 })
 export default class AMarker extends Vue {
     @Prop() marker!: AMarkerType
     @Prop() icon!: IconMarker
+    get itsOpen() {
+        const value = true
+        if(value) {
+            return true
+        }
+        else return false
+    }
 }
 </script>
 

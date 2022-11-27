27798a48597ba1489c98688afc5c04c83f3a09a0
dasco69 <claude.proaudiovisuel@gmail.com>
2022-11-27 10:25:55 +0100

    style: change import

--- pages/login.vue
+++ pages/login.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue } from 'nuxt-property-decorator'
 import OLogin from '@/components/Organisms/login/OLogin.vue'
 
 @Component({

2e8ab9e29775895797ca498b103e78db69580620
dasco69 <claude.proaudiovisuel@gmail.com>
2022-11-28 21:02:48 +0100

    feat: add molecule signup

---
+++ components/Molecules/login/MSignup.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="mx-34 my-20 flex items-center flex-col">
+        <ATitleBase class="text-center mb-12" colorText="blueDark" :title="phraseLog.titlePage" />
+        <AInputField
+            :type="form[0].type"
+            :label="form[0].label"
+            :placeholder="form[0].placeholder"
+            @update="(item) => { loginData.username = item}"
+        />
+        <AInputField
+            :type="form[1].type"
+            :label="form[1].label"
+            :placeholder="form[1].placeholder"
+            @update="(item) => { loginData.email = item}"
+        />
+        <AInputField
+            :type="form[2].type"
+            :label="form[2].label"
+            :placeholder="form[2].placeholder"
+            @update="(item) => { loginData.password = item}"
+        />
+         <!-- If mobile -->
+         <div class="my-5 md:hidden inline-flex">
+            <p class="text-blueDark">{{ phraseLog.firstPart }}</p>
+            <NuxtLink :to="phraseLog.pathRedirect">
+                <p class="ml-2 text-blueLight font-bold">{{ phraseLog.secondPart }}</p>
+            </NuxtLink>
+         </div>
+         <AButtonBase 
+            @click.native="$emit('submit', loginData)" 
+            :title="button" 
+            class="shadow-button" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+//Atoms
+import AInputField from '@/components/Atoms/input/AInputFiled.vue'
+import ATitleBase from '@/components/Atoms/title/ATitleBase.vue'
+import ATextAreaField from '@/components/Atoms/textarea/ATextAreaField.vue'
+import AButtonBase from '@/components/Atoms/button/AButtonBase.vue'
+//Store
+import { Login, Signup } from '~/store/user'
+//Interface
+export interface inputForm {
+    type: string
+    label: string
+    placeHolder: string
+    update: (a: string [ number]) =>  string | number
+
+}
+@Component({
+    components: {
+        ATitleBase,
+        AInputField,
+        ATextAreaField,
+        AButtonBase
+    }
+})
+export default class MSignup extends Vue {
+    @Prop() form!: inputForm[]
+    @Prop() button!: string
+
+    public loginData = {} as Signup
+
+    /**
+     * @getter phraseLog
+     * @description
+     * Change phrase or word when path route is different
+     * HTML compoennt is a pattern
+     * Only for login and sigup
+     * 
+     */
+    get phraseLog() {
+        if (this.$route.path === '/signup')
+        return {
+                    titlePage: 'Inscrivez-vous!',
+                    firstPart: 'Déjà un compte ?',
+                    secondPart: 'Connectez-vous',
+                    pathRedirect: '/login'
+                }
+    }
+
+
+}
+</script>
+
+<style scoped></style>
